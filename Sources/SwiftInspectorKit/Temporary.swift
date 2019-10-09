fe42a2bf7f29713be85cbc69a0857f079fea0029
Francisco Diaz <patchdiaz@gmail.com>
2019-10-09 11:34:14 -0700

    Add files to help with setting up Temporary files

---
+++ Sources/SwiftInspectorKit/Temporary.swift
@@ -0,0 +1,32 @@
+// Created by Francisco Diaz on 10/6/19.
+// Copyright © 2019 Airbnb Inc. All rights reserved.
+
+import Foundation
+
+struct Temporary {
+  /// Creates a Swift file in a temporary directory
+  ///
+  /// - Parameter content: A String representing the contents of the Swift file
+  /// - Returns: The file URL where the created file is stored
+  static func makeSwiftFile(withContent content: String) throws -> URL {
+    let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
+                                    isDirectory: true)
+    let fileName = UUID().uuidString + ".swift"
+    let temporaryFileURL =
+      temporaryDirectoryURL.appendingPathComponent(fileName)
+
+    let data = content.data(using: .utf8)! // Can't fail
+    try data.write(to: temporaryFileURL,
+                   options: .atomic)
+    
+    return temporaryFileURL
+  }
+
+
+  /// Removes a file in the specified location
+  ///
+  /// - Parameter fileURL: The location of the file to remove
+  static func removeFile(at fileURL: URL) throws {
+    try FileManager.default.removeItem(at: fileURL)
+  }
+}
