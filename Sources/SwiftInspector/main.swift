c5a07d26da1624b6a82cd55ab5f6202bf5ff624b
Francisco Diaz <patchdiaz@gmail.com>
2019-10-11 14:58:48 -0700

    Add simple main.swift using Commandant

--- Sources/SwiftInspector/main.swift
+++ Sources/SwiftInspector/main.swift
@@ -1,1 +1,11 @@
-print("Hello, world!")
+import Commandant
+import Foundation
+
+let registry = CommandRegistry<Error>()
+
+let helpCommand = HelpCommand(registry: registry)
+registry.register(helpCommand)
+
+registry.main(defaultVerb: helpCommand.verb) { error in
+  fputs("\(error)\n", stderr)
+}
