3191872c246ff2a8ce94dd1863b5aec5490a31bf
Francisco Diaz <patchdiaz@gmail.com>
2019-10-12 17:30:53 -0700

    Make it easy to create a temporary folder

--- Tests/SwiftInspectorKitTests/TemporarySpec.swift
+++ Tests/SwiftInspectorKitTests/TemporarySpec.swift
@@ -11,7 +11,7 @@ final class TemporarySpec: QuickSpec {
   override func spec() {
     describe("makeSwiftFile") {
       it("creates a file") {
-        guard let savedURL = try? Temporary.makeSwiftFile(withContent: "abc") else {
+        guard let savedURL = try? Temporary.makeSwiftFile(content: "abc") else {
           return fail("Something went wrong when creating a temporary file. This shouldn't fail.")
         }
 
@@ -20,7 +20,7 @@ final class TemporarySpec: QuickSpec {
 
       it("saves the correct content") {
         let content = "protocol Some { }"
-        guard let savedURL = try? Temporary.makeSwiftFile(withContent: content) else {
+        guard let savedURL = try? Temporary.makeSwiftFile(content: content) else {
           return fail("Something went wrong when creating a temporary file. This shouldn't fail.")
         }
 
@@ -29,17 +29,50 @@ final class TemporarySpec: QuickSpec {
       }
     }
 
-    describe("removeFile") {
+    describe("makeFolder") {
+      it("creates a directory") {
+        guard let folderURL = try? Temporary.makeFolder(name: "abc") else {
+          return fail("Something went wrong when creating a temporary folder. This shouldn't fail.")
+        }
+
+        expect(FileManager.default.fileExists(atPath: folderURL.path)) == true
+      }
+    }
+
+    describe("cleanFolder") {
+      it("deletes all files from a directory") {
+        guard let folderURL = try? Temporary.makeFolder(name: "abc") else {
+          return fail("Something went wrong when creating a temporary folder. This shouldn't fail.")
+        }
+
+        expect(FileManager.default.fileExists(atPath: folderURL.path)) == true
+      }
+    }
+
+    describe("removeItem") {
       it("deletes a existing file") {
-        guard let savedURL = try? Temporary.makeSwiftFile(withContent: "abc") else {
+        guard let savedURL = try? Temporary.makeSwiftFile(content: "abc") else {
           return fail("Something went wrong when creating a temporary file. This shouldn't fail.")
         }
 
         expect(FileManager.default.fileExists(atPath: savedURL.path)) == true
 
-        try? Temporary.removeFile(at: savedURL)
+        try? Temporary.removeItem(at: savedURL)
 
         expect(FileManager.default.fileExists(atPath: savedURL.path)) == false
+      }
+
+      it("deletes an existing folder") {
+        guard let folderURL = try? Temporary.makeFolder(name: "abc") else {
+          return fail("Something went wrong when creating a temporary folder. This shouldn't fail.")
+        }
+
+        expect(FileManager.default.fileExists(atPath: folderURL.path)) == true
+
+        try? Temporary.removeItem(at: folderURL)
+
+        expect(FileManager.default.fileExists(atPath: folderURL.path)) == false
+
       }
     }
   }
