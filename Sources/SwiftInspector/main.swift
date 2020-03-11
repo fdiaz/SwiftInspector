a63027767d69dd0f8a0ad9e0fb25851d17e9ad41
Francisco Diaz <patchdiaz@gmail.com>
2020-03-11 12:00:36 -0700

    Add top command

--- Sources/SwiftInspector/main.swift
+++ Sources/SwiftInspector/main.swift
@@ -1,15 +1,1 @@
-import Commandant
-import Foundation
-import SwiftInspectorKit
-
-let registry = CommandRegistry<Error>()
-registry.register(TypeConformanceCommand())
-registry.register(StaticUsageCommand())
-
-let helpCommand = HelpCommand(registry: registry)
-registry.register(helpCommand)
-
-registry.main(defaultVerb: helpCommand.verb) { error in
-  // Using fputs instead of print to log to standard error
-  fputs("\(error)\n", stderr)
-}
+InspectorCommand.main()
