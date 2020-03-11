4f4da313ccc355026d3f75ccf86e053e0e586882
Francisco Diaz <patchdiaz@gmail.com>
2020-03-11 11:56:25 -0700

    Remove Commandant

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
     .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
-    .package(url: "https://github.com/Carthage/Commandant.git", from: "0.17.0"),
     .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
     .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
   ],
@@ -24,7 +23,6 @@ let package = Package(
       name: "SwiftInspector",
       dependencies: [
         "SwiftInspectorKit",
-        "Commandant",
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
     ]),
     .testTarget(
