0f765d234fe1de8bcc63c10a61f4a2a61d29eaf8
Francisco Diaz <patchdiaz@gmail.com>
2020-03-14 11:18:03 -0700

    Add option to output full information of odule

--- Sources/SwiftInspector/Commands/ImportsCommand.swift
+++ Sources/SwiftInspector/Commands/ImportsCommand.swift
@@ -14,6 +14,9 @@ final class ImportsCommand: ParsableCommand {
   @Option()
   var path: String
 
+  @Option(default: .main, help: OutputMode.help)
+  var mode: OutputMode
+
   /// Runs the command
   func run() throws {
     let cachedSyntaxTree = CachedSyntaxTree()
@@ -35,8 +38,41 @@ final class ImportsCommand: ParsableCommand {
 
    // Outputs to standard output
   private func output(from imports: [ImportStatement]) {
-    let output = imports.map { $0.mainModule }.joined(separator: "\n")
+    let output = imports.map { outputString(from: $0) }.joined(separator: "\n")
     print(output)
+  }
+
+  private func outputString(from statement: ImportStatement) -> String {
+    switch mode {
+    case .main:
+      return statement.mainModule
+    case .full:
+      let attribute = statement.attribute.isEmpty ? "" : "@\(statement.attribute)"
+      var module = statement.mainModule
+      if !statement.submodule.isEmpty {
+        module += ".\(statement.submodule)"
+      }
+      return "\(attribute) \(statement.kind) \(module)"
+    }
   }
 }
 
+enum OutputMode: String, ExpressibleByArgument, Decodable {
+  /// Outputs the main module name only
+  case main
+
+  /// Outputs the full import statement
+  case full
+}
+
+extension OutputMode {
+  static var help: ArgumentHelp {
+    ArgumentHelp("The granularity of what's outputted",
+                 discussion: """
+                             If main is passed, it only outputs the main module on the import,
+                             ignoring the attribute, kind and submodule.
+                             If full is passed, it outputs every property on the import.
+                             """
+    )
+  }
+}
