a12e7637a8120e5c9ee2ac0a6761767ae0ebca64
Francisco Diaz <patchdiaz@gmail.com>
2020-04-03 15:28:32 -0700

    Handle tuple initializer

--- Sources/SwiftInspectorCore/Tests/InitializerAnalyzerSpec.swift
+++ Sources/SwiftInspectorCore/Tests/InitializerAnalyzerSpec.swift
@@ -139,6 +139,43 @@ final class InitializerAnalyzerSpec: QuickSpec {
             ]
           }
         }
+
+        context("with a tuple") {
+          beforeEach {
+            let content = """
+            public final class FakeType {
+              init(someTyple: (String, Int)) {}
+            }
+            """
+            fileURL = try? Temporary.makeFile(content: content)
+          }
+
+          it("returns all the types in the array") {
+            let result = try? sut.analyze(fileURL: fileURL)
+            expect(result?.first?.parameters) == [
+              InitializerStatement.Parameter(name: "someTyple", typeNames: ["String", "Int"]),
+            ]
+          }
+        }
+
+        context("with a closure") {
+          beforeEach {
+            let content = """
+            public final class FakeType {
+              init(someTyple: (String, Int)) {}
+            }
+            """
+            fileURL = try? Temporary.makeFile(content: content)
+          }
+
+          it("returns all the types in the array") {
+            let result = try? sut.analyze(fileURL: fileURL)
+            expect(result?.first?.parameters) == [
+              InitializerStatement.Parameter(name: "someTyple", typeNames: ["String", "Int"]),
+            ]
+          }
+        }
+
       }
 
       context("with multiple initializers") {
