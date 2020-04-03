afe698a7505731c3dae719673a4b17fcadde53ac
Francisco Diaz <fdiaz@users.noreply.github.com>
2020-04-03 16:03:34 -0700

    Add a way to filter the initializer by argument names (#22)
    * Adds a way to filter by argument names
    
    * Update Sources/SwiftInspectorCommands/InitializerCommand.swift
    
    Co-Authored-By: Dan Federman <dfed@me.com>
    
    * Handle tuple initializer
    
    * argumentName -> parameterName
    
    Co-authored-by: Dan Federman <dfed@me.com>

--- Sources/SwiftInspectorCommands/Tests/InitializerCommandSpec.swift
+++ Sources/SwiftInspectorCommands/Tests/InitializerCommandSpec.swift
@@ -87,6 +87,33 @@ final class InitializerCommandSpec: QuickSpec {
         }
       }
 
+      context("when parameter-name is passed") {
+        var fileURL: URL!
+
+        beforeEach {
+          fileURL = try? Temporary.makeFile(content: """
+          final class Some {
+            init(some: String, someInt: Int) {}
+          }
+          """)
+        }
+
+        it("filters out the initializers that don't have the parameter names") {
+          let result = try? TestTask.run(withArguments: ["initializer", "--path", fileURL.path, "--name", "Some", "--parameter-name", "AnotherType"])
+          expect(result?.outputMessage).toNot(contain("String"))
+        }
+
+        it("returns the initializers that have the same names") {
+          let result = try? TestTask.run(withArguments: ["initializer", "--path", fileURL.path, "--name", "Some", "--parameter-name", "some", "someInt"])
+          expect(result?.outputMessage).to(contain("String Int"))
+        }
+
+        it("returns the initializers that have the same names in different order") {
+          let result = try? TestTask.run(withArguments: ["initializer", "--path", fileURL.path, "--name", "Some", "--parameter-name", "someInt", "some"])
+          expect(result?.outputMessage).to(contain("String Int"))
+        }
+      }
+
     }
   }
 }
