04e734d872e4d3daaba1ab8e30f5fd5b316a540d
Michael Bachand <bachand.michael@gmail.com>
2020-04-04 14:11:54 -0700

    Test uses beEmpty() instead of testing ""

--- Sources/SwiftInspectorCommands/Tests/TypeLocationCommandSpec.swift
+++ Sources/SwiftInspectorCommands/Tests/TypeLocationCommandSpec.swift
@@ -130,7 +130,7 @@ final class TypeLocationCommandSpec: QuickSpec {
               pathURL = try? Temporary.makeFile(content: contents)
 
               let result = try? TestTypeLocationTask.run(path: pathURL.path, name: "Bar")
-              expect(result?.outputMessage).to(equal(""))
+              expect(result?.outputMessage).to(beEmpty())
             }
           }
 
