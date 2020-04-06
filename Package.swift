710d635c22d06a135244b5cc934298f7bc2de1aa
Francisco Diaz <patchdiaz@gmail.com>
2020-04-06 13:14:42 -0700

    Update ArgumentParser to 0.0.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .library(name: "SwiftInspectorCore", targets: ["SwiftInspectorCore"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.4")),
     .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50100.0")),
     .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
     .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
