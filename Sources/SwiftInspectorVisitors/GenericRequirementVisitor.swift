d85feb35fbd9ec02e4669b4c02e4e6384b13f6a2
Dan Federman <dan.federman@airbnb.com>
2021-01-27 13:34:13 -0800

    Comments are good y'all

--- Sources/SwiftInspectorVisitors/GenericRequirementVisitor.swift
+++ Sources/SwiftInspectorVisitors/GenericRequirementVisitor.swift
@@ -56,12 +56,14 @@ public struct GenericRequirement: Codable, Equatable {
   }
 
   init(node: SameTypeRequirementSyntax) {
+    // There is no known case where these identifiers can not be cast to SimpleTypeIdentifierSyntax.
     leftType = node.leftTypeIdentifier.as(SimpleTypeIdentifierSyntax.self)!.name.text
     rightType = node.rightTypeIdentifier.as(SimpleTypeIdentifierSyntax.self)!.name.text
     relationship = .equals
   }
 
   init(node: ConformanceRequirementSyntax) {
+    // There is no known case where these identifiers can not be cast to SimpleTypeIdentifierSyntax.
     leftType = node.leftTypeIdentifier.as(SimpleTypeIdentifierSyntax.self)!.name.text
     rightType = node.rightTypeIdentifier.as(SimpleTypeIdentifierSyntax.self)!.name.text
     relationship = .conformsTo
