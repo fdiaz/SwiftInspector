472d283a70846c87a48f059e43072be83cfcce87
Dan Federman <dan.federman@airbnb.com>
2021-01-28 18:20:31 -0800

    Create ProtocolVisitor (#59)

---
+++ Sources/SwiftInspectorVisitors/ProtocolVisitor.swift
@@ -0,0 +1,83 @@
+// Created by Dan Federman on 1/28/21.
+//
+// Copyright © 2021 Dan Federman
+//
+// Distributed under the MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+import SwiftSyntax
+
+public final class ProtocolVisitor: SyntaxVisitor {
+
+  public private(set) var protocolInfo: ProtocolInfo?
+
+  public override func visit(_ node: ProtocolDeclSyntax) -> SyntaxVisitorContinueKind {
+
+    guard !hasFinishedParsingProtocol else {
+      assertionFailure("Encountered more than one top-level protocol. This is a usage error: a single ProtocolVisitor instance should start walking only over a node of type `ProtocolDeclSyntax`")
+      return .skipChildren
+    }
+
+    let typeInheritanceVisitor = TypeInheritanceVisitor()
+    typeInheritanceVisitor.walk(node)
+    let genericRequirementsVisitor = GenericRequirementVisitor()
+    genericRequirementsVisitor.walk(node)
+
+    protocolInfo = ProtocolInfo(
+      name: node.identifier.text,
+      inheritsFromTypes: typeInheritanceVisitor.inheritsFromTypes,
+      genericRequirements: genericRequirementsVisitor.genericRequirements)
+
+    // We don't (yet) care about what is in this protocol. When we start looking for
+    // properties on this protocol we'll need to start visiting children.
+    return .skipChildren
+  }
+
+  public override func visitPost(_ node: ProtocolDeclSyntax) {
+    hasFinishedParsingProtocol = true
+  }
+
+  public override func visit(_ node: StructDeclSyntax) -> SyntaxVisitorContinueKind {
+    assertionFailure("Encountered a top-level struct. This is a usage error: a single ProtocolVisitor instance should start walking only over a node of type `ProtocolDeclSyntax`")
+    return .skipChildren
+  }
+
+  public override func visit(_ node: ClassDeclSyntax) -> SyntaxVisitorContinueKind {
+    assertionFailure("Encountered a top-level class. This is a usage error: a single ProtocolVisitor instance should start walking only over a node of type `ProtocolDeclSyntax`")
+    return .skipChildren
+  }
+
+  public override func visit(_ node: EnumDeclSyntax) -> SyntaxVisitorContinueKind {
+    assertionFailure("Encountered a top-level enum. This is a usage error: a single ProtocolVisitor instance should start walking only over a node of type `ProtocolDeclSyntax`")
+    return .skipChildren
+  }
+
+  // MARK: Private
+
+  private var hasFinishedParsingProtocol = false
+}
+
+public struct ProtocolInfo: Codable, Equatable {
+  public let name: String
+  public let inheritsFromTypes: [String]
+  public let genericRequirements: [GenericRequirement]
+  // TODO: also find and expose properties on a protocol
+}
---
+++ Sources/SwiftInspectorVisitors/Tests/ProtocolVisitorSpec.swift
@@ -0,0 +1,258 @@
+// Created by Dan Federman on 1/28/21.
+//
+// Copyright © 2021 Dan Federman
+//
+// Distributed under the MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+import Nimble
+import Quick
+import SwiftInspectorTestHelpers
+
+@testable import SwiftInspectorVisitors
+
+final class ProtocolVisitorSpec: QuickSpec {
+  private var sut = ProtocolVisitor()
+
+  override func spec() {
+    beforeEach {
+      self.sut = ProtocolVisitor()
+    }
+
+    describe("visit(_:)") {
+      context("visiting a single protocol declaration") {
+        context("with no conformance") {
+          it("finds the type name") {
+            let content = """
+              public protocol SomeProtocol {}
+              """
+
+            try VisitorExecutor.walkVisitor(
+              self.sut,
+              overContent: content)
+
+            let protocolInfo = self.sut.protocolInfo
+            expect(protocolInfo?.name) == "SomeProtocol"
+            expect(protocolInfo?.inheritsFromTypes) == []
+            expect(protocolInfo?.genericRequirements) == []
+          }
+        }
+
+        context("with a single type conformance") {
+          it("finds the type name") {
+            let content = """
+              public protocol SomeProtocol: Equatable {}
+              """
+
+            try VisitorExecutor.walkVisitor(
+              self.sut,
+              overContent: content)
+
+            let protocolInfo = self.sut.protocolInfo
+            expect(protocolInfo?.name) == "SomeProtocol"
+            expect(protocolInfo?.inheritsFromTypes) == ["Equatable"]
+            expect(protocolInfo?.genericRequirements) == []
+          }
+        }
+
+        context("with multiple type conformances") {
+          it("finds the type name") {
+            let content = """
+              public protocol SomeProtocol: Foo, Bar {}
+              """
+
+            try VisitorExecutor.walkVisitor(
+              self.sut,
+              overContent: content)
+
+            let protocolInfo = self.sut.protocolInfo
+            expect(protocolInfo?.name) == "SomeProtocol"
+            expect(protocolInfo?.inheritsFromTypes) == ["Foo", "Bar"]
+            expect(protocolInfo?.genericRequirements) == []
+          }
+        }
+
+        context("with a single type conformance and generic equals constraint") {
+          it("finds both the type conforamnce and generic constraint") {
+            let content = """
+              public protocol SomeProtocol: Collection where Element == Int {}
+              """
+
+            try VisitorExecutor.walkVisitor(
+              self.sut,
+              overContent: content)
+
+            let protocolInfo = self.sut.protocolInfo
+            expect(protocolInfo?.name) == "SomeProtocol"
+            expect(protocolInfo?.inheritsFromTypes) == ["Collection"]
+            expect(protocolInfo?.genericRequirements.first?.leftType) == "Element"
+            expect(protocolInfo?.genericRequirements.first?.rightType) == "Int"
+            expect(protocolInfo?.genericRequirements.first?.relationship) == .equals
+          }
+
+          context("with a single type conformance and multiple generic constraints") {
+            it("finds alll generic constraints") {
+              let content = """
+              public protocol SomeProtocol: Transformer where
+                Input == Int,
+                Output: AnyObject
+              {}
+              """
+
+              try VisitorExecutor.walkVisitor(
+                self.sut,
+                overContent: content)
+
+              let protocolInfo = self.sut.protocolInfo
+              expect(protocolInfo?.name) == "SomeProtocol"
+              expect(protocolInfo?.genericRequirements.first?.leftType) == "Input"
+              expect(protocolInfo?.genericRequirements.first?.rightType) == "Int"
+              expect(protocolInfo?.genericRequirements.first?.relationship) == .equals
+              expect(protocolInfo?.genericRequirements.last?.leftType) == "Output"
+              expect(protocolInfo?.genericRequirements.last?.rightType) == "AnyObject"
+              expect(protocolInfo?.genericRequirements.last?.relationship) == .conformsTo
+            }
+          }
+
+          context("with a single type conformance and generic conformance constraint") {
+            it("finds both the type conforamnce and generic constraint") {
+              let content = """
+              public protocol SomeProtocol: Collection where Element: Int {}
+              """
+
+              try VisitorExecutor.walkVisitor(
+                self.sut,
+                overContent: content)
+
+              let protocolInfo = self.sut.protocolInfo
+              expect(protocolInfo?.name) == "SomeProtocol"
+              expect(protocolInfo?.inheritsFromTypes) == ["Collection"]
+              expect(protocolInfo?.genericRequirements.first?.leftType) == "Element"
+              expect(protocolInfo?.genericRequirements.first?.rightType) == "Int"
+              expect(protocolInfo?.genericRequirements.first?.relationship) == .conformsTo
+            }
+          }
+        }
+
+        context("visiting a code block with multiple top-level declarations") {
+          context("with multiple protocols") {
+            it("asserts") {
+              let content = """
+            public protocol FooProtocol {}
+            public protocol BarProtocol {}
+            """
+
+              // The ProtocolVisitor is only meant to be used over a single protocol.
+              // Using a ProtocolVisitor over a block that has multiple top-level
+              // protocols is API misuse.
+              expect(try VisitorExecutor.walkVisitor(
+                      self.sut,
+                      overContent: content))
+                .to(throwAssertion())
+            }
+          }
+
+          context("with a top-level class after a protocol") {
+            it("asserts") {
+              let content = """
+            public protocol FooProtocol {}
+            public protocol FooClass {}
+            """
+
+              // The ProtocolVisitor is only meant to be used over a single protocol.
+              // Using a ProtocolVisitor over a block that has a top-level class
+              // is API misuse.
+              expect(try VisitorExecutor.walkVisitor(
+                      self.sut,
+                      overContent: content))
+                .to(throwAssertion())
+            }
+          }
+
+          context("with a top-level enum after a protocol") {
+            it("asserts") {
+              let content = """
+            public protocol FooProtocol {}
+            public protocol FooEnum {}
+            """
+
+              // The ProtocolVisitor is only meant to be used over a single protocol.
+              // Using a ProtocolVisitor over a block that has a top-level enum
+              // is API misuse.
+              expect(try VisitorExecutor.walkVisitor(
+                      self.sut,
+                      overContent: content))
+                .to(throwAssertion())
+            }
+          }
+        }
+
+        context("visiting a code block with a top-level struct declaration") {
+          it("asserts") {
+            let content = """
+            public struct FooStruct {}
+            """
+
+            // The ProtocolVisitor is only meant to be used over a single protocol.
+            // Using a ProtocolVisitor over a block that has a top-level class
+            // is API misuse.
+            expect(try VisitorExecutor.walkVisitor(
+                    self.sut,
+                    overContent: content))
+              .to(throwAssertion())
+          }
+        }
+
+        context("visiting a code block with a top-level class declaration") {
+          it("asserts") {
+            let content = """
+            public class FooClass {}
+            """
+
+            // The ProtocolVisitor is only meant to be used over a single protocol.
+            // Using a ProtocolVisitor over a block that has a top-level class
+            // is API misuse.
+            expect(try VisitorExecutor.walkVisitor(
+                    self.sut,
+                    overContent: content))
+              .to(throwAssertion())
+          }
+        }
+
+        context("visiting a code block with a top-level enum declaration") {
+          it("asserts") {
+            let content = """
+            public enum FooEnum {}
+            """
+
+            // The ProtocolVisitor is only meant to be used over a single protocol.
+            // Using a ProtocolVisitor over a block that has a top-level enum
+            // is API misuse.
+            expect(try VisitorExecutor.walkVisitor(
+                    self.sut,
+                    overContent: content))
+              .to(throwAssertion())
+          }
+        }
+      }
+    }
+  }
+}
