85a44f88cb48d9d421988d9e28f499bcc86a9d98
Francisco Diaz <patchdiaz@gmail.com>
2021-09-14 12:26:37 -0700

    Add Package for Swift 5.5

---
+++ Package@swift-5.5.swift
@@ -0,0 +1,87 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "SwiftInspector",
+  platforms: [
+    .macOS(.v10_13)
+  ],
+  products: [
+    .executable(name: "swiftinspector", targets: ["SwiftInspector"]),
+    .library(name: "SwiftInspectorAnalyzers", targets: ["SwiftInspectorAnalyzers"]),
+    .library(name: "SwiftInspectorVisitors", targets: ["SwiftInspectorVisitors"]),
+    .library(name: "SwiftInspectorTestHelpers", targets: ["SwiftInspectorTestHelpers"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.1")),
+    .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .branch("release/5.5")),
+    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.1")),
+    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
+  ],
+  targets: [
+    .executableTarget(
+      name: "SwiftInspector",
+      dependencies: [
+        "SwiftInspectorCommands",
+    ]),
+
+    .target(
+      name: "SwiftInspectorCommands",
+      dependencies: [
+        "SwiftInspectorAnalyzers",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+    ], exclude: ["Tests"]),
+    .testTarget(
+      name: "SwiftInspectorCommandsTests",
+      dependencies: [
+        "SwiftInspectorCommands",
+        "SwiftInspectorTestHelpers",
+        "Nimble",
+        "Quick",
+    ], path: "Sources/SwiftInspectorCommands/Tests"),
+
+    .target(
+      name: "SwiftInspectorAnalyzers",
+      dependencies: [
+        "SwiftSyntax",
+        "SwiftInspectorVisitors",
+      ],
+      exclude: ["Tests"]),
+    .testTarget(
+      name: "SwiftInspectorAnalyzersTests",
+      dependencies: [
+        "SwiftInspectorAnalyzers",
+        "SwiftInspectorTestHelpers",
+        "SwiftInspectorVisitors",
+        "Nimble",
+        "Quick",
+    ], path: "Sources/SwiftInspectorAnalyzers/Tests"),
+
+    .target(
+      name: "SwiftInspectorTestHelpers",
+      dependencies: ["SwiftSyntax"],
+      exclude: ["Tests"]),
+    .testTarget(
+      name: "SwiftInspectorTestHelpersTests",
+      dependencies: [
+        "SwiftInspectorTestHelpers",
+        "Nimble",
+        "Quick",
+    ], path: "Sources/SwiftInspectorTestHelpers/Tests"),
+
+    .target(
+      name: "SwiftInspectorVisitors",
+      dependencies: ["SwiftSyntax"],
+      exclude: ["Tests"]),
+    .testTarget(
+      name: "SwiftInspectorVisitorsTests",
+      dependencies: [
+        "SwiftInspectorTestHelpers",
+        "SwiftInspectorVisitors",
+        "Nimble",
+        "Quick",
+    ], path: "Sources/SwiftInspectorVisitors/Tests"),
+  ]
+)
