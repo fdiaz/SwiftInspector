476a45401afd1579dfaeaf9d5f0033cfd39af9cb
Dan Federman <dan.federman@airbnb.com>
2021-11-04 21:23:06 -0700

    Assert when we encounter an attributed type with no specifier nor attributes

--- Sources/SwiftInspectorVisitors/TypeDescription.swift
+++ Sources/SwiftInspectorVisitors/TypeDescription.swift
@@ -124,7 +124,8 @@ public enum TypeDescription: Codable, Hashable {
         // Reference manual: https://docs.swift.org/swift-book/ReferenceManual/Types.html#grammar_type
         return "\(specifier) \(attributesFromList(attributes)) \(type.asSource)"
       case (.none, .none):
-        // This case likely represents an error.
+        // This case represents an error and is unreachable because never store an attributed type with neither a specifier nor attributes.
+        assertionFailureOrPostNotification("Creating source for an attributed type with no attributes or specifiers")
         return type.asSource
       }
     case let .array(element):
@@ -366,12 +367,19 @@ extension TypeSyntax {
         isType: typeIdentifier.typeOrProtocol.text == "Type")
 
     } else if let typeIdentifier = self.as(AttributedTypeSyntax.self) {
-      return .attributed(
-        typeIdentifier.baseType.typeDescription,
-        specifier: typeIdentifier.specifier?.text,
-        attributes: typeIdentifier.attributes?.compactMap {
-          $0.as(AttributeSyntax.self)?.attributeName.text
-        })
+      let specifier = typeIdentifier.specifier?.text
+      let attributes = typeIdentifier.attributes?.compactMap {
+        $0.as(AttributeSyntax.self)?.attributeName.text
+      }
+      if specifier == nil && attributes == nil {
+        assertionFailureOrPostNotification("Encountered an attributed type with no attributes or specifiers")
+        return typeIdentifier.baseType.typeDescription
+      } else {
+        return .attributed(
+          typeIdentifier.baseType.typeDescription,
+          specifier: specifier,
+          attributes: attributes)
+      }
 
     } else if let typeIdentifier = self.as(ArrayTypeSyntax.self) {
       return .array(element: typeIdentifier.elementType.typeDescription)
