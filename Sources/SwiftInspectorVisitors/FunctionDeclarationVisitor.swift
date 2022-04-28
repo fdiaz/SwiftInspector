5c29de67cfbfdcdb00e478e982d6625db7fffea3
Dan Federman <dan.federman@airbnb.com>
2022-04-28 16:40:26 -0700

    Capture function parameters (#129)

--- Sources/SwiftInspectorVisitors/FunctionDeclarationVisitor.swift
+++ Sources/SwiftInspectorVisitors/FunctionDeclarationVisitor.swift
@@ -5,7 +5,10 @@ public final class FunctionDeclarationVisitor: SyntaxVisitor {
     let name = node.identifier.withoutTrivia().description
     let functionSignatureVisitor = FunctionSignatureVisitor()
     functionSignatureVisitor.walk(node)
-    let info = FunctionDeclarationInfo(name: name, returnType: functionSignatureVisitor.returnType)
+    let info = FunctionDeclarationInfo(
+      name: name,
+      arguments: functionSignatureVisitor.arguments,
+      returnType: functionSignatureVisitor.returnType)
     functionDeclarations.append(info)
     return .skipChildren
   }
@@ -14,16 +17,42 @@ public final class FunctionDeclarationVisitor: SyntaxVisitor {
 }
 
 fileprivate final class FunctionSignatureVisitor: SyntaxVisitor {
+  override func visit(_ node: FunctionParameterSyntax) -> SyntaxVisitorContinueKind {
+    guard
+      let argumentLabelName = node.firstName?.text,
+      let type = node.type?.typeDescription
+    else { return .skipChildren }
+
+    appendArgument(argumentLabelName: argumentLabelName, type: type)
+    return .skipChildren
+  }
   override func visit(_ node: ReturnClauseSyntax) -> SyntaxVisitorContinueKind {
     returnType = node.returnType.typeDescription
     return .skipChildren
   }
 
+  fileprivate func appendArgument(argumentLabelName: String, type: TypeDescription) {
+    var arguments = self.arguments ?? []
+    arguments.append(.init(argumentLabelName: argumentLabelName, type: type))
+    self.arguments = arguments
+  }
+
+  fileprivate var arguments: [FunctionDeclarationInfo.ArgumentInfo]?
   fileprivate var returnType: TypeDescription?
 }
 
 public struct FunctionDeclarationInfo: Codable, Hashable {
   public let name: String
+  public let arguments: [ArgumentInfo]?
   public let returnType: TypeDescription?
-  // TODO: Add parameters and argument labels
+
+  /// A convenience for creating a selector string that can be reference in Objective-C code.
+  public var selectorName: String {
+    "\(name)(\((arguments ?? []).map { "\($0.argumentLabelName):" }.joined()))"
+  }
+
+  public struct ArgumentInfo: Codable, Hashable {
+    public let argumentLabelName: String
+    public let type: TypeDescription
+  }
 }
--- Sources/SwiftInspectorVisitors/Tests/FunctionDeclarationVisitorSpec.swift
+++ Sources/SwiftInspectorVisitors/Tests/FunctionDeclarationVisitorSpec.swift
@@ -28,6 +28,10 @@ final class FunctionDeclarationVisitorSpec: QuickSpec {
         it("finds the function name") {
           expect(self.sut.functionDeclarations.first?.name) == "greet"
         }
+        it("finds the function parameter") {
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.argumentLabelName) == "person"
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.type) == .simple(name: "String")
+        }
         it("finds the return type") {
           expect(self.sut.functionDeclarations.first?.returnType) == TypeDescription.simple(name: "String")
         }
@@ -46,6 +50,10 @@ final class FunctionDeclarationVisitorSpec: QuickSpec {
 
         it("finds the function name") {
           expect(self.sut.functionDeclarations.first?.name) == "printWithoutCounting"
+        }
+        it("finds the function parameter") {
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.argumentLabelName) == "string"
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.type) == .simple(name: "String")
         }
         it("sets the return type as nil") {
           expect(self.sut.functionDeclarations.first?.returnType).to(beNil())
@@ -74,6 +82,10 @@ final class FunctionDeclarationVisitorSpec: QuickSpec {
 
         it("finds the function name") {
           expect(self.sut.functionDeclarations.first?.name) == "minMax"
+        }
+        it("finds the function parameter") {
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.argumentLabelName) == "array"
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.type) == .array(element: .simple(name: "Int"))
         }
         it("finds the tuple return value") {
           expect(self.sut.functionDeclarations.first?.returnType) == .tuple([.simple(name: "Int"), .simple(name: "Int")])
@@ -109,15 +121,97 @@ final class FunctionDeclarationVisitorSpec: QuickSpec {
           expect(self.sut.functionDeclarations.count) == 2
         }
         it("finds the first function") {
-          expect(self.sut.functionDeclarations.first?.name) == "minMax"
-          expect(self.sut.functionDeclarations.first?.returnType) == .tuple([.simple(name: "Int"), .simple(name: "Int")])
+          let firstFunction = self.sut.functionDeclarations.first
+          expect(firstFunction?.name) == "minMax"
+          expect(firstFunction?.arguments?.first?.argumentLabelName) == "array"
+          expect(firstFunction?.arguments?.first?.type) == .array(element: .simple(name: "Int"))
+          expect(firstFunction?.returnType) == .tuple([.simple(name: "Int"), .simple(name: "Int")])
         }
         it("finds the last function") {
-          expect(self.sut.functionDeclarations.last?.name) == "printWithoutCounting"
-          expect(self.sut.functionDeclarations.last?.returnType).to(beNil())
+          let lastFunction = self.sut.functionDeclarations.last
+          expect(lastFunction?.name) == "printWithoutCounting"
+          expect(lastFunction?.arguments?.first?.argumentLabelName) == "string"
+          expect(lastFunction?.arguments?.first?.type) == .simple(name: "String")
+          expect(lastFunction?.returnType).to(beNil())
         }
       }
-      
+
+      context("function with no parameters") {
+        beforeEach {
+          let content = """
+            func helloWorld() {
+                print("Hello, world!")
+            }
+            """
+
+          try? self.sut.walkContent(content)
+        }
+
+        it("finds no arguments") {
+          expect(self.sut.functionDeclarations.first?.arguments).to(beNil())
+        }
+      }
+
+      context("function with no argument label") {
+        beforeEach {
+          let content = """
+            func print(_ string: String) {
+                let _ = printAndCount(string: string)
+            }
+            """
+
+          try? self.sut.walkContent(content)
+        }
+
+        it("finds the parameter label") {
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.argumentLabelName) == "_"
+        }
+        it("calculates the selectorName") {
+          expect(self.sut.functionDeclarations.first?.selectorName) == "print(_:)"
+        }
+      }
+
+      context("function with an argument label that differs from the parameter name") {
+        beforeEach {
+          let content = """
+            func append(argumentLabelName parameterName: String) {
+              // ...
+            }
+            """
+
+          try? self.sut.walkContent(content)
+        }
+
+        it("finds the parameter label") {
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.argumentLabelName) == "argumentLabelName"
+        }
+        it("calculates the selectorName") {
+          expect(self.sut.functionDeclarations.first?.selectorName) == "append(argumentLabelName:)"
+        }
+      }
+
+      context("function with multiple parameters") {
+        beforeEach {
+          let content = """
+            func append(argumentLabelName parameterName: String, type: TypeDescription) {
+              // ...
+            }
+            """
+
+          try? self.sut.walkContent(content)
+        }
+
+        it("finds the correct number of parameters") {
+          expect(self.sut.functionDeclarations.first?.arguments?.count) == 2
+        }
+        it("finds the parameter labels") {
+          expect(self.sut.functionDeclarations.first?.arguments?.first?.argumentLabelName) == "argumentLabelName"
+          expect(self.sut.functionDeclarations.first?.arguments?.last?.argumentLabelName) == "type"
+        }
+        it("calculates the selectorName") {
+          expect(self.sut.functionDeclarations.first?.selectorName) == "append(argumentLabelName:type:)"
+        }
+      }
     }
   }
 }
