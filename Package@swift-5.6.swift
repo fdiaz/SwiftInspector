146b886b719bcd56f6f4cafb04297b5d22186986
Dan Federman <dan.federman@airbnb.com>
2022-04-27 17:00:16 -0700

    Minimum macOS support of v12

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "SwiftInspector",
   platforms: [
-    .macOS(.v10_13)
+    .macOS(.v12)
   ],
   products: [
     .executable(name: "swiftinspector", targets: ["SwiftInspector"]),
